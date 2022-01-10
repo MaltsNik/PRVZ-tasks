12430c59560337fb397e0f6d60e9ec17c099a52d
Никита Мальцев <nikolas141996@gmail.com>
2022-01-10 08:51:29 +0300

    first commit

---
+++ src/main/java/com/nikita/prvztasks/Main.java
@@ -0,0 +1,12 @@
+package com.nikita.prvztasks;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new Server((request, responce) -> {
+            return "<html><body><h1>Hello, naive</h1></body></html>";
+
+        }).bootstrap();
+    }
+
+}
